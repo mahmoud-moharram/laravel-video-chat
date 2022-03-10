20c859f1bfd77efe2d20b78af4a9c1e808eb2869
mahmoud-moharram <57960669+mahmoud-moharram@users.noreply.github.com>
2022-03-10 20:20:39 +0200

    Add files via upload

---
+++ app/Http/Controllers/VideoChatController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\StartVideoChat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class VideoChatController extends Controller
+{
+    //
+
+    public function callUser(Request $request)
+    {
+        $data['userToCall'] = $request->user_to_call;
+        $data['signalData'] = $request->signal_data;
+        $data['from'] = Auth::id();
+        $data['type'] = 'incomingCall';
+
+        broadcast(new StartVideoChat($data))->toOthers();
+    }
+    public function endCall(Request $request)
+    {
+        $data['from'] = Auth::id();
+        $data['type'] = 'endCall';
+
+        broadcast(new StartVideoChat($data))->toOthers();
+    }
+
+    public function acceptCall(Request $request)
+    {
+        $data['signal'] = $request->signal;
+        $data['to'] = $request->to;
+        $data['type'] = 'callAccepted';
+        broadcast(new StartVideoChat($data))->toOthers();
+    }
+}
